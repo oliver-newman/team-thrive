3723160a8d264b9a17c3d1fee0f62d0453d17c07
Oliver Newman <oliver@whmsi.com>
2016-10-28 14:01:35 -0400

    Edit help and home, add about static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    assert_response :success # :success = "200 OK" (HTTP status code)
   end
 
   test "should get help" do
@@ -11,4 +11,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
