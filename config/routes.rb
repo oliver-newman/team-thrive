b763ce59386c3a80b4660ad1ec8c15dbf87f2519
Oliver Newman <oliver@whmsi.com>
2016-12-01 15:56:00 -0500

    Set up signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/feed', to: 'static_pages#feed'
   get '/signup', to: 'users#new'
+
+  resources :users
 end
