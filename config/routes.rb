3e5310ec973f725b9a8472f1edc4154dad182cf2
Oliver Newman <oliver@whmsi.com>
2016-12-02 20:39:27 -0500

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/feed', to: 'static_pages#feed'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
