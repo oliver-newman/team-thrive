6d829955fb67a08095ca03b4652a65f334b85c48
Oliver Newman <oliver@whmsi.com>
2016-12-07 12:01:53 -0500

    Add remember_token to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  attr_accessor :remember_token
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
@@ -8,18 +8,29 @@ class User < ApplicationRecord
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password # BCrypt
   validates :password, presence: true, length: { minimum: 6 }
 
-  has_secure_password # BCrypt
+  before_save { email.downcase! }
 
   def full_name
     "#{first_name} #{last_name}"
   end
 
-  # Returns hash digest of the given string
-  def User.digest(string)
+  # Returns hash digest of the given string.
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token for persistent cookies.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = new_token
+    update_attribute(:remember_digest, digest(remember_token))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202011759) do
+ActiveRecord::Schema.define(version: 20161207152407) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161202011759) do
     t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
