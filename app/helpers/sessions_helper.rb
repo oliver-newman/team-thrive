86ff81551c503b85d739e84875973605c1f54b7c
Oliver Newman <oliver@whmsi.com>
2016-12-11 21:47:48 -0500

    Implement forgetting of persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,45 @@
 module SessionsHelper
-  # Start a session when a user logs in
+  # Starts a session (when a user logs in).
   def log_in(user)
     # Uses the rails session method to save key-value pair as a cookie
     session[:user_id] = user.id
   end
 
-  # Returns the current logged-in user (if any)
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+  # Remembers user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns true if user is logged in, false otherwise
+  # Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Returns true if user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
-  # Logs out the current user
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,20 +17,36 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
-  # Returns hash digest of the given string.
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Returns a random token for persistent cookies.
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-
+  # Saves/updates a remember token digest for a user's current persistent 
+  # session, and a remember token for the current temporary session.
   def remember
-    self.remember_token = new_token
-    update_attribute(:remember_digest, digest(remember_token))
+    self.remember_token = self.class.new_token
+    update_attribute(:remember_digest, self.class.digest(remember_token))
+  end
+
+  # Returns true if given token matches the user's current digest (i.e., if
+  # the user's account is being accessed from the computer from which the user's
+  # current peristent session was initiated).
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  class << self
+    # Returns hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token for persistent cookies.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
