fbb55f1c03f7f2773a932e8960ab9d81dbd0fb44
Oliver Newman <oliver@whmsi.com>
2016-12-18 21:18:14 -0500

    Allow strava_id as param in user create request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,8 +49,8 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, 
-                                   :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :strava_id,
+                                   :password, :password_confirmation)
     end
 
     # before_action filters
