49ac97b6e4ed4ad9ba32e354cda6b592f2fda724
Oliver Newman <oliver@whmsi.com>
2016-12-18 21:00:36 -0500

    Fix signup link in login page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(first_name: "Example",
+             last_name: "User",
+             email: "user@example.com",
+             strava_id: 5882007,
+             password:  "password",
+             password_confirmation: "password",
+             admin: true)
+
+99.times do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = "#{first_name.downcase}#{i+1}@example.com"
+  strava_id = 5882007
+  password = "password"
+  User.create!(first_name: first_name,
+               last_name: last_name,
+               email: email,
+               strava_id: strava_id,
+               password: password,
+               password_confirmation: password)
+end
