ee5a88e64973b43b01fd2f4ef7441bb0ca5bf648
Oliver Newman <oliver@whmsi.com>
2016-12-30 18:54:29 -0500

    Fix migration of sport from string to enum

--- db/migrate/20161230020253_change_activity_sport_type_to_enum.rb
+++ db/migrate/20161230020253_change_activity_sport_type_to_enum.rb
@@ -1,5 +1,5 @@
 class ChangeActivitySportTypeToEnum < ActiveRecord::Migration[5.0]
   def change
-    change_column :activities, :sport, :integer, default: 0
+    change_column :activities, :sport, 'integer USING CAST(sport AS integer)'
   end
 end
