3b54d2de9bbb0b61427d941b3b023b14c3f1b8d5
Oliver Newman <oliver@whmsi.com>
2016-12-30 18:33:43 -0500

    Avoid using dotenv in production

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+# Use dotenv to load secrets
+Dotenv::Railtie.load unless Rails.env.production?
 
 HOSTNAME = ENV['HOSTNAME']
 
