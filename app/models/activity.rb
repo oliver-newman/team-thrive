aa9052c4753851e6841a4eb340764cd85a7a6c40
Oliver Newman <oliver@whmsi.com>
2016-12-30 15:57:25 -0500

    Fix tentative map_url method for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,9 @@
 class Activity < ApplicationRecord
   default_scope -> { order(start_date: :desc) }
 
+  BIKE_DOLLARS_PER_METER = 0.00006215
+  RUN_DOLLARS_PER_METER = 0.0002486
+
   enum sport: { run: 0, ride: 1 }
 
   belongs_to :user
@@ -22,5 +25,11 @@ class Activity < ApplicationRecord
   # Returns the URL of the corresponding activity on Strava.
   def strava_url
     "https://www.strava.com/activities/#{strava_activity_id}"
+  end
+
+  def map_url(size)
+    "https://www.maps.googleapis.com/maps/api/staticmap?size=#{size}x#{size}" +
+      "&path=weight:3%color:blue%7:Cenc:#{CGI::escape(summary_polyline)}" +
+      "&key=#{Rails.application.config.GOOGLE_MAPS_KEY}"
   end
 end
