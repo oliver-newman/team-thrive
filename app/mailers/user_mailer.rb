b26190aaa47299fd97fd740ab92e454cbc09fc99
Oliver Newman <oliver@whmsi.com>
2017-01-03 22:57:58 -0500

    Remove password reset mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,14 +9,4 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Activate your TeamThrive account"
   end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset(user)
-    @user = user
-    mail to: user.email, subject: "Reset your TeamThrive account password"
-  end
 end
