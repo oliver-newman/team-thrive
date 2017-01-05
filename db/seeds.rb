061bf437cdee9e76bb0e0d52ebc9083dc09c0095
Oliver Newman <oliver@whmsi.com>
2017-01-04 19:03:14 -0500

    Restructure users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
 # Users
-User.create!(first_name: "Oliver",
-             last_name: "Newman",
-             email: "onewman1@swarthmore.edu",
-             strava_id: 5882007,
-             admin: true)
+User.create!(
+  first_name: "Oliver",
+  last_name: "Newman",
+  email: "onewman1@swarthmore.edu",
+  strava_id: 5882007,
+  strava_token: Faker::Crypto.sha1,
+  admin: true
+)
 
 99.times do |i|
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = "#{first_name.downcase}#{i+1}@example.com"
-  strava_id = 5882007
-  User.create!(first_name: first_name,
-               last_name: last_name,
-               email: email,
-               strava_id: strava_id)
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "user#{i+1}@example.com",
+    strava_token: Faker::Crypto.sha1,
+    strava_id: i
+  )
 end
 
 
@@ -21,14 +23,15 @@ end
 users = User.order(:created_at).take(6)
 50.times do
   users.each do |user|
-    user.activities.create!(title: Faker::Lorem.sentence, 
-                            sport: ["run", "ride"].sample,
-                            distance: rand(10000),
-                            elevation_gain: rand(1000),
-                            moving_time: rand(10000),
-                            strava_activity_id: 808191805,
-                            start_date: Faker::Date.between(5.years.ago,
-                                                            Time.zone.now))
+    user.activities.create!(
+      title: Faker::Lorem.sentence[0..-2], 
+      sport: ["run", "ride"].sample,
+      distance: rand(10000),
+      elevation_gain: rand(1000),
+      moving_time: rand(10000),
+      strava_activity_id: 808191805,
+      start_date: Faker::Date.between(5.years.ago, Time.zone.now)
+    )
   end
 end
 
