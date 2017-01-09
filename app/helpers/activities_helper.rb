585b94d494af8a48a0b5d77ba9a447c049252ad6
Oliver Newman <oliver@whmsi.com>
2017-01-08 20:28:26 -0500

    Add rough polyline maps

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -4,18 +4,23 @@ module ActivitiesHelper
     "https://www.strava.com/activities/#{activity_id}"
   end
 
-  # Returns an image tag for an icon representing either a sport or a meal
+  # Returns an image tag for an icon representing either a sport or a meal.
   def icon_for(subject)
     image_tag("#{subject}.png", alt: subject, class: "icon #{subject}-icon")
   end
 
-  def polyline_image(polyline, size)
-    polyline = CGI::escape(polyline.to_s) # CGI::escape(polyline.to_s)
-    p polyline
-    image_tag("https://maps.googleapis.com/maps/api/staticmap?" +
-                "size=#{size}x#{size}" +
-                "&path=weight:3%color:orange%7:Cenc:#{polyline}" +
-                "&key=#{Rails.application.secrets.GOOGLE_MAPS_KEY}",
+  def polyline_map_url(polyline, size, color)
+    polyline = CGI::escape(polyline.to_s)
+    "https://maps.googleapis.com/maps/api/staticmap?" +
+      "size=#{size}x#{size}" + 
+      "&path=weight:3%7Ccolor:#{color}%7Cenc:#{polyline}" +
+      "&key=#{Rails.application.secrets.GOOGLE_MAPS_KEY}"
+  end
+
+  # Returns an image tag for a Google Maps image with the GPS polyline of the
+  # activity.
+  def polyline_image(polyline, size = 150, color = "blue")
+    image_tag(polyline_map_url(polyline, size: size, color: color),
               alt: "Activity map")
   end
 end
